4466c8fe2a43acb47b9913c175df67c793f36d59
SherbekFayzullayev <sherbekcode.2328@gmail.com>
2025-08-19 13:19:27 +0500

    001 change

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp());
+
+}
+
+class LibraryApp extends StatelessWidget {
+  const LibraryApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(),
+    );
+  }
+}
